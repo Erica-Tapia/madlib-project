a928df37f971cc43a98b292fdcc7719d1f34a87c
Erica L Tapia <95728435+Erica-Tapia@users.noreply.github.com>
2022-03-16 15:43:16 -0500

    Create Madlib-Project.py
    Created the python file and added code for samples of "how" we will print the concatenated statement, planning to use the last option.

---
+++ Madlib-Project.py
@@ -0,0 +1,12 @@
+#string concatenation
+
+#sample code and suppose you want someone to subscribe to a youtuber
+#create a varible for youtuber
+
+youtuber = "Sample"
+
+#print the message of subscribing and youtuber name
+#3 different formating ways to print the message
+print("subscribe to " + youtuber)
+print("subscribe to {}".format(youtuber))
+print(f"subscribe to {youtuber}") 
